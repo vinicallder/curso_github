19c728bd23b52a5535fe2b8c91358f3d655a34ee
vinicallder <111135311+vinicallder@users.noreply.github.com>
2022-08-26 11:33:04 -0300

    Add files via upload

---
+++ Sem Título1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h> //Biblioteca de entrada/saída por periféricos padrão
+#include<conio.h> //Biblioteca para manipulação de caracteres
+#include<stdlib.h>//Biblioteca padrão para o uso de funções do sistema
+int main () {//Programa principal
+// Declaração de Variáveis
+	int num_alunos, num_alunas
+//Cabeçalho
+	printf("Universidade de Bras%clia\n, 161");
+	printf("Disciplina: 113913 - ICC \n");
+	printf("Aluno(a): \n\n\n")
+}
